245a2b45e0cdf963de02e7f838445b06d6498bb8
Wendell Ribeiro <wrs1001@gmail.com>
2026-01-06 00:55:27 -0300

    aprendendo_formulario

--- 4-View/view/app/controllers/welcome_controller.rb
+++ 4-View/view/app/controllers/welcome_controller.rb
@@ -1,11 +1,25 @@
 class WelcomeController < ApplicationController
+  before_action :initialize_produtos, only: [:index]
   def index
-    @produtos = [
-      { nome: "Camiseta", preco: 19.99},
-      { nome: "Calça Jeans", preco: 39.99},
-      { nome: "Tênis", preco: 59.99},
-      { nome: "PS 5", preco: 4500.00}
+    # @produtos = [
+    #   { nome: "Camiseta", preco: 19.99},
+    #   { nome: "Calça Jeans", preco: 39.99},
+    #   { nome: "Tênis", preco: 59.99},
+    #   { nome: "PS 5", preco: 4500.00}
+    # ]
+    @produtos = session[:produtos]
+    puts @produtos.inspect
+  end
+  def adicionar_produto
+    produto = {nome: params[:nome], preco: params[:preco].to_f}
+    session[:produtos] << produto
+    redirect_to root_path
+  end
 
-    ]
+  private
+
+  def initialize_produtos
+    session[:produtos] ||= []
+    @produtos = session[:produtos]
   end
 end
--- 4-View/view/config/routes.rb
+++ 4-View/view/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get "test", to: "welcome#index"
   root "welcome#index"
+  post "adicionar_produto", to: "welcome#adicionar_produto"
 end
